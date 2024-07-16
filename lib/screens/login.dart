d95cec4ff418e3dc43faf9e465705a52e2485c58
nameoftheuser1 <103398819+nameoftheuser1@users.noreply.github.com>
2024-07-16 23:20:03 +0800

    login

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,5 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:medmart/screens/register.dart';
+import 'package:http/http.dart' as http;
+import 'package:medmart/services/user.dart';
+
 
 class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
@@ -10,8 +15,27 @@ class Login extends StatefulWidget {
 
 class _LoginState extends State<Login> {
   final _formKey = GlobalKey<FormState>();
-  String _username = '';
+  String _email = '';
   String _password = '';
+
+  login(User user) async {
+    final response = await http.post(
+      Uri.parse('http://10.0.2.2:8080/api/v1/auth/login'),
+      headers: <String, String>{
+        'Content-type' : 'application/json; charset=UTF-8'
+      },
+      body: jsonEncode(<String,  dynamic>{
+        'usernameOrEmail' : user.email,
+        'password' : user.password
+      }),
+    );
+    if (response.statusCode == 200) {
+      return 'Login successful';
+    } else {
+      return 'Login failed: ${response.body}';
+    }
+
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -58,11 +82,12 @@ class _LoginState extends State<Login> {
                             return null;
                           },
                           onSaved: (value) {
-                            _username = value!;
+                            _email = value!;
                           },
                         ),
                         const SizedBox(height: 20.0),
                         TextFormField(
+                          obscureText: true,
                           decoration: const InputDecoration(
                             labelText: 'Password',
                             prefixIcon: Icon(Icons.lock),
@@ -86,17 +111,23 @@ class _LoginState extends State<Login> {
                             SizedBox(
                               width: 200.0,
                               child: ElevatedButton(
-                                style: const ButtonStyle(
-                                  backgroundColor: WidgetStatePropertyAll(
-                                      Color.fromARGB(255, 73, 105, 76)),
+                                style: ButtonStyle(
+                                  backgroundColor: MaterialStateProperty.all(
+                                    Color.fromARGB(255, 73, 105, 76),
+                                  ),
                                 ),
                                 onPressed: () {
                                   if (_formKey.currentState!.validate()) {
                                     _formKey.currentState!.save();
-                                    // Handle the login logic here
+                                    User user = User(username: '', email: _email, password: _password);
+                                    login(user);
                                     ScaffoldMessenger.of(context).showSnackBar(
-                                      const SnackBar(
-                                          content: Text('Logging in')),
+                                      SnackBar(content: Text("Logging in")),
+                                    );
+                                    Navigator.pushNamed(context, '/');
+                                  } else {
+                                    ScaffoldMessenger.of(context).showSnackBar(
+                                      SnackBar(content: Text("not accepted")),
                                     );
                                   }
                                 },
